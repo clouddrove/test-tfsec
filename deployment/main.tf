4d597d9c959b5d0342efc4aae6faf9e0e279287f
Deepak Verma <anmol@clouddrove.com>
2023-06-16 19:18:34 +0530

    feat: added workflow

---
+++ deployment/main.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+  features {}
+}
+
+# module "resource_group" {
+#   source = "terraform/resource-group/azure"
+#   version = "1.0.2"
+
+#   environment = "tested"
+#   label_order = ["name", "environment", ]
+
+#   name     = "example"
+#   location = "North Europe"
+
+#   #resource lock
+#   resource_lock_enabled = false
+#   lock_level            = "CanNotDelete"
+# }
+
+module "resource-group" {
+  source  = "clouddrove/resource-group/azure"
+  version = "1.0.2"
+
+  environment = "tested"
+  label_order = ["name", "environment", ]
+
+  name     = "example"
+  location = "North Europe"
+
+  #resource lock
+  resource_lock_enabled = false
+  lock_level            = "CanNotDelete"
+}
