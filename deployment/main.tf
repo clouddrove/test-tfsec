57272eebb201fe978375ec94e059203e7f911afe
Deepak Verma <anmol@clouddrove.com>
2023-07-12 15:42:04 +0530

    feat: new aws workflow added

--- deployment/main.tf
+++ deployment/main.tf
@@ -1,12 +1,33 @@
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
 
-# module "resource_group" {
-#   source = "terraform/resource-group/azure"
+# # module "resource_group" {
+# #   source = "terraform/resource-group/azure"
+# #   version = "1.0.2"
+
+# #   environment = "tested"
+# #   label_order = ["name", "environment", ]
+
+# #   name     = "example"
+# #   location = "North Europe"
+
+# #   #resource lock
+# #   resource_lock_enabled = false
+# #   lock_level            = "CanNotDelete"
+# # }
+
+# terraform {
+#   backend "azurerm" {
+#     container_name = "terraform"
+#   }
+# }
+
+# module "resource-group" {
+#   source  = "clouddrove/resource-group/azure"
 #   version = "1.0.2"
 
-#   environment = "tested"
+#   environment = var.environment
 #   label_order = ["name", "environment", ]
 
 #   name     = "example"
@@ -17,24 +38,25 @@ provider "azurerm" {
 #   lock_level            = "CanNotDelete"
 # }
 
-terraform {
-  backend "azurerm" {
-    container_name = "terraform"
-  }
+####----------------------------------------------------------------------------------
+## Provider block added, Use the Amazon Web Services (AWS) provider to interact with the many resources supported by AWS.
+####----------------------------------------------------------------------------------
+provider "aws" {
+  region = "us-east-1"
 }
 
-module "resource-group" {
-  source  = "clouddrove/resource-group/azure"
-  version = "1.0.2"
+####----------------------------------------------------------------------------------
+##  Module      : KEY PAIR
+## Terraform module for generating or importing an SSH public key file into AWS.
+####----------------------------------------------------------------------------------
+module "keypair" {
+  source = "./../"
 
+  name        = "key"
   environment = var.environment
-  label_order = ["name", "environment", ]
+  label_order = ["environment", "name"]
 
-  name     = "example"
-  location = "North Europe"
-
-  #resource lock
-  resource_lock_enabled = false
-  lock_level            = "CanNotDelete"
+  public_key                 = ""
+  create_private_key_enabled = true
+  enable_key_pair            = true
 }
-
